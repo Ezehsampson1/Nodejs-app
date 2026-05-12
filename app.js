59cc430479c6d03f6c6f174e59e269ca83159c98
Sampson <Ezehsampson1@gmail.com>
2026-05-12 21:00:19 +0100

    bump version to 2.0.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: "Engr Sampson's DevOps pipeline is live! oshey!!!", version: '1.0.1' });
+  res.json({ message: "Engr Sampson's DevOps pipeline is live! oshey!!!", version: '2.0.0' });
 });
 
 app.get('/health', (req, res) => {
